88b96cd39e0f45534a46fa0ce722bd242d707a19
dihbgs <dihborgesdr@gmail.com>
2024-02-17 00:19:29 -0300

    feat: add support for animation

--- src/graphics/Canvas.java
+++ src/graphics/Canvas.java
@@ -1,62 +1,98 @@
 package src.graphics;
 
-import javax.swing.JComponent;
+import javax.swing.Timer;
+import javax.swing.JPanel;
+
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
-public class Canvas extends JComponent {
-    private int width;
-    private int height;
+public class Canvas extends JPanel {
+	private final int frameRate = 24;
 
-    public Canvas() {
-        this(400, 400);
-    }
+	private int width;
+	private int height;
 
-    public Canvas(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
+	private static long elapsedTime;
+	private static long currentTime;
+	private static long previousTime;
 
-    public void resize(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
+	public Canvas() {
+		this(400, 400);
 
-    public void resize(int size) {
-        this.width = size;
-        this.height = size;
-    }
+		elapsedTime = 1;
+		previousTime = System.currentTimeMillis();
 
-    public int getWidth() {
-        return this.width;
-    }
+		Timer timer = new Timer(1000 / frameRate, (event) -> gameLoop());
 
-    public int getHeight() {
-        return this.height;
-    }
+		timer.start();
+	}
 
-    public void paintComponent(Graphics context) {
-        Graphics2D graphics = (Graphics2D) context;
-        
-        super.paintComponent(graphics);
-        this.setPreferredSize(new Dimension(1024, this.getHeight()));
-    }
+	public Canvas(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
 
-    public Dimension getMaximumSize() {
-        return getPreferredSize();
-    }
+	public void resize(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
 
-    public Dimension getMinimumSize() {
-        return getPreferredSize();
-    }
+	public void resize(int size) {
+		this.width = size;
+		this.height = size;
+	}
 
-    public void setPreferredSize(Dimension preferredSize) {
-        this.width = preferredSize.width;
-        this.height = preferredSize.height;
-    }
+	public int getWidth() {
+		return this.width;
+	}
 
-    public Dimension getPreferredSize() {
-        return new Dimension(this.width, this.height);
-    }
+	public int getHeight() {
+		return this.height;
+	}
+
+	public void paintComponent(Graphics context) {
+		Graphics2D graphics = (Graphics2D) context;
+
+		super.paintComponent(graphics);
+		this.setPreferredSize(new Dimension(1024, this.getHeight()));
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		int x = (int) (Math.random() * this.getWidth());
+		int y = (int) (Math.random() * this.getHeight());
+
+		graphics.setColor(java.awt.Color.WHITE);
+		graphics.fillRect(0, 0, this.getWidth(), this.getHeight());
+		graphics.setColor(java.awt.Color.BLACK);
+		graphics.fillRect(x, y, 50, 50);
+		graphics.drawString("Frame rate: " + 1000 / elapsedTime + " fps", 32, 32);
+	}
+
+	private void gameLoop() {
+		currentTime = System.currentTimeMillis();
+		elapsedTime = currentTime - previousTime;
+
+		if (elapsedTime >= 1000 / frameRate) {
+			previousTime = currentTime;
+			repaint();
+		}
+	}
+
+	public Dimension getMaximumSize() {
+		return getPreferredSize();
+	}
+
+	public Dimension getMinimumSize() {
+		return getPreferredSize();
+	}
+
+	public void setPreferredSize(Dimension preferredSize) {
+		this.width = preferredSize.width;
+		this.height = preferredSize.height;
+	}
+
+	public Dimension getPreferredSize() {
+		return new Dimension(this.width, this.height);
+	}
 }
